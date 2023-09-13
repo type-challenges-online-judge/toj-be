8836151a95db02c0381d81f4c41e445484cddfa5
junghyunbak <jeong5728@gmail.com>
2023-09-14 01:30:56 +0900

    chore: /api swagger 웹 페이지 명세에서 제거

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { BaseAPIDocument } from './swagger.document';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ProblemModule } from './modules/problem/problem.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const config = new BaseAPIDocument().initializeOptions();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [ProblemModule],
+  });
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
