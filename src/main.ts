233a7c6efe52b5b0cfca887bf2e090957c6c0ef3
junghyunbak <jeong5728@gmail.com>
2023-09-14 01:16:07 +0900

    feat: swagger 모듈 설치

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
+import { BaseAPIDocument } from './swagger.document';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new BaseAPIDocument().initializeOptions();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
