84ecabdc457cf0270a62fa2d9ddc14ed1de0863b
junghyunbak <jeong5728@gmail.com>
2023-09-18 00:05:07 +0900

    feat: 미들웨어 & 가드에 사용 될 custom decorator 제작

---
+++ src/decorators/index.ts
@@ -0,0 +1,40 @@
+import type { Request } from 'express';
+
+export function Middleware(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor,
+) {
+  const method = descriptor.value;
+
+  descriptor.value = function (req: Request) {
+    const args = target.extractors.map(({ type, value }) => {
+      switch (type) {
+        case 'query':
+          return req.query[value];
+        case 'request':
+          return req;
+      }
+    });
+
+    method(...args);
+  };
+}
+
+export function Query(property: string) {
+  return function (target: any, propertyKey: string, parameterIndex: number) {
+    if (!target.extractors) {
+      target.extractors = [];
+    }
+
+    target.extractors.unshift({ type: 'query', value: property });
+  };
+}
+
+export function Req(target: any, propertyKey: string, parameterIndex: number) {
+  if (!target.extractors) {
+    target.extractors = [];
+  }
+
+  target.extractors.unshift({ type: 'request', value: null });
+}
