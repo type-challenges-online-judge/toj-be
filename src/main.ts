923dac44ac8a6d285d9b40be0bae697ef0f2febe
junghyunbak <jeong5728@gmail.com>
2023-09-17 17:35:58 +0900

    chore: global prefix에 예외 경로 추가

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,36 @@ import { AppModule } from '@/app.module';
 import { BaseAPIDocument } from './swagger.document';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ProblemModule } from './modules/problem/problem.module';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  /**
+   * 루트 경로를 제외한 모든 경로에 `/api` 접두사 추가
+   */
+  app.setGlobalPrefix('api', {
+    exclude: [
+      {
+        path: '/',
+        method: RequestMethod.ALL,
+      },
+    ],
+  });
 
+  /**
+   * swagger 관련 설정
+   */
   const config = new BaseAPIDocument().initializeOptions();
   const document = SwaggerModule.createDocument(app, config, {
     include: [ProblemModule],
   });
   SwaggerModule.setup('api', app, document);
 
+  /**
+   * 3000번 포트로 앱 실행
+   */
   await app.listen(3000);
 }
+
 bootstrap();
