6038085ff7975ad941e966d5405bbd1d1df7412d
junghyunbak <jeong5728@gmail.com>
2023-09-18 12:13:31 +0900

    chore: User 스키마 구조 변경

--- src/models/entity/User.ts
+++ src/models/entity/User.ts
@@ -7,14 +7,11 @@ export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('int')
+  @Column({ type: 'int', unique: true })
   snsId: number;
 
   @Column({ length: 255 })
   name: string;
-
-  @Column({ length: 320 })
-  email: string;
 
   @OneToMany(() => SubmitCode, (submitCode) => submitCode.user)
   submitCode: SubmitCode[];
