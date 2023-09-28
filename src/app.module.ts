cb2ab2be47d28405fa91c713476844e6730aa4c5
junghyunbak <jeong5728@gmail.com>
2023-09-28 18:10:43 +0900

    refactor: 사용되지 않는 app 컨트롤러, 서비스, 테스트코드 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '@/app.controller';
 import { ProblemModule } from '@/modules/problem/problem.module';
-import { AppService } from '@/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from '@/models/data-source';
 import { AuthModule } from './modules/auth/auth.module';
@@ -14,7 +12,7 @@ import { UserModule } from './modules/user/user.module';
     AuthModule,
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
